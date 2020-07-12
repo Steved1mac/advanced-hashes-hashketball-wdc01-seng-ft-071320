db7d1a8c38546abe2aeb1d38a38fc7a18e56072f
Steven McAllister <steved1mac@gmail.com>
2020-07-12 02:40:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -174,9 +174,8 @@ end
   end 
     
 def player_stats(player_name)
-  new_array = []
   game_hash.each do |key, value|
-    value.each do |element|
+    value[:players] do |element|
     
    
    
